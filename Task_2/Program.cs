d9c034b7ade07a6ec085d9af297cfe2a7d480ccd
Liandon <bilinskiy-01@mail.ru>
2022-12-11 13:06:05 +0600

    1

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Добрый день!");
+Console.WriteLine("Введите 1-ое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    Console.WriteLine("первое число больше второго");
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+  }
+  if (num1<num2)
+  {
+    Console.WriteLine("второе число больше первого");
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+  }
